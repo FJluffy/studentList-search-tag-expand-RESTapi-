32f31500f67368871a2acc96ab489d602c9a4979
unknown <jaydenchen0000@gmail.com>
2021-02-22 21:18:19 -0500

    studentLlist

---
+++ src/App.js
@@ -0,0 +1,106 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Student from "./components/Student";
+import "./App.css";
+
+function App() {
+  const [students, setStudents] = useState([]);
+  const [searchStudents, setSearchStudents] = useState([]);
+  const [name, setName] = useState("");
+  const [tag, setTag] = useState("");
+
+  useEffect(() => {
+    axios.get("https://www.hatchways.io/api/assessment/students").then(res => {
+      const studentsData = res.data.students;
+      studentsData.forEach(s => {
+        const avg = `${s.grades.reduce((a, b) => Number(a) + Number(b)) /
+          s.grades.length}%`;
+        const student = {
+          pic: s.pic,
+          firstname: s.firstName,
+          lastname: s.lastName,
+          name: `${s.firstName} ${s.lastName}`,
+          company: s.company,
+          email: s.email,
+          skill: s.skill,
+          grades: s.grades,
+          average: avg,
+          key: s.id,
+          tags: []
+        };
+        setStudents(students => [...students, student]);
+        setSearchStudents(students => [...students, student]);
+      });
+    });
+  }, []);
+
+  useEffect(() => {
+    console.log(students[0]);
+    var result = students.filter(s => s.name.toLowerCase().includes(name));
+    if (tag.length !== 0) {
+      function checkTag(tags) {
+        console.log(tags);
+        var hasTag;
+        for (var i = 0; i < tags.length; i++) {
+          if (tags[i].includes(tag)) {
+            hasTag = true;
+            console.log(tags[i]);
+            break;
+          } else {
+            hasTag = false;
+          }
+        }
+        return hasTag;
+      }
+      var filteredResult = Array.from(result);
+
+      result = filteredResult.filter(s => checkTag(s.tags));
+      console.log(result);
+    }
+    setSearchStudents(result);
+  }, [name, tag]);
+
+  const NameSearch = e => {
+    const newName = e.target.value.toLowerCase();
+    setName(newName);
+  };
+
+  const TagSearch = e => {
+    setTag(e.target.value.toLowerCase());
+  };
+
+  const addTag = (id, tags) => {
+    const newStudents = Array.from(students);
+    newStudents[id - 1].tags = tags;
+    setStudents(newStudents);
+  };
+  return (
+    <div className="container">
+      <h1>Students System</h1>
+      <form className="search">
+        <input
+          id="name-input"
+          type="text"
+          value={name}
+          placeholder="Search Name"
+          onChange={NameSearch}
+        />
+        <br />
+        <input
+          id="tag-input"
+          type="text"
+          value={tag}
+          placeholder="Search tag"
+          onChange={TagSearch}
+        />
+      </form>
+      <div className="studentsContainer">
+        {searchStudents.map(s => (
+          <Student key={s.key} student={s} addTag={addTag} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Student.js
@@ -0,0 +1,91 @@
+import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus, faMinus } from "@fortawesome/free-solid-svg-icons";
+
+export class Student extends Component {
+  constructor() {
+    super();
+    this.state = {
+      showGrade: false,
+      tag: "",
+      tags: []
+    };
+    this.gradeShow = this.gradeShow.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.addStudentTag = this.addStudentTag.bind(this);
+  }
+  componentDidMount() {
+    this.setState({ tags: this.state.tags.concat(this.props.student.tags) });
+  }
+  gradeShow() {
+    this.setState({ showGrade: !this.state.showGrade });
+  }
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+  addStudentTag(e, key) {
+    e.preventDefault();
+    this.setState(
+      {
+        tags: this.state.tags.concat(this.state.tag)
+      },
+      () => {
+        this.props.addTag(key, this.state.tags);
+      }
+    );
+    this.setState({ tag: "" });
+  }
+  render() {
+    const plus = <FontAwesomeIcon icon={faPlus} />;
+    const minus = <FontAwesomeIcon icon={faMinus} />;
+    return (
+      <div className="studentContainer">
+        <button onClick={this.gradeShow} className="expand-btn">
+          {this.state.showGrade === true ? (
+            <div>{minus}</div>
+          ) : (
+              <div>{plus}</div>
+            )}
+        </button>
+        <img src={this.props.student.pic} alt="profile" />
+        <h2>{this.props.student.name}</h2>
+        <h3>Email:{this.props.student.email}</h3>
+        <h3>Company:{this.props.student.company}</h3>
+        <h3>Skill:{this.props.student.skill}</h3>
+        <h3>Average:{this.props.student.average}</h3>
+        {this.props.student.tags.length === 0 ? null : (
+          <div>
+            {this.props.student.tags.map(tag => (
+              <label className="tags" key={tag}>{tag}</label>
+            ))}
+          </div>
+        )}
+        {this.state.showGrade === true ? (
+          <div>
+            <div>
+              {this.props.student.grades.map((grade, index) => (
+                <h5 key={index}>
+                  <span>Test{index + 1}: </span> {grade}%
+                </h5>
+              ))}
+            </div>
+            <form onSubmit={e => this.addStudentTag(e, this.props.student.key)}>
+              <input
+                className="add-tag-input"
+                type="text"
+                value={this.state.tag}
+                name="tag"
+                onChange={e => this.handleChange(e)}
+              />
+              <button type="submit">New Tag</button>
+            </form>
+          </div>
+        ) : null}
+      </div>
+    );
+  }
+}
+
+export default Student;
